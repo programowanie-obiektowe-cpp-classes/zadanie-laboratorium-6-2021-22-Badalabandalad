f0b9e8249005efd96f713891b43c139cdbee4b6b
292882 (Przemysław Bus) <01133092@pw.edu.pl>
2022-01-06 16:07:05 +0100

    v4

--- foo.hpp
+++ foo.hpp
@@ -10,7 +10,11 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
 
-    // Twoja implementacja tutaj
+    std::vector< char > ret_v(people.size());
+    std::for_each(people.begin(), people.end(), [](auto& h) { h.birthday(); });
+    std::transform(people.crbegin(), people.crend(), ret_v.begin(), [](const auto& h) {
+        return !(h.isMonster()) ? 'y' : 'n';
+    });
 
     return ret_v;
 }
