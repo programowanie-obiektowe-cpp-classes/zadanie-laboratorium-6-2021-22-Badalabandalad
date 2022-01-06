73f205c1224b9951e784cc3de2fda4aab5ef8233
292882 (Przemysław Bus) <01133092@pw.edu.pl>
2022-01-06 16:10:39 +0100

    v5

--- foo.hpp
+++ foo.hpp
@@ -1,19 +1,25 @@
 #pragma once
 
 #include "Human.hpp"
-
 #include <algorithm>
+#include <numeric>
 #include <list>
 #include <vector>
+using namespace std;
 
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
 
-    std::vector< char > ret_v(people.size());
-    std::for_each(people.begin(), people.end(), [](auto& h) { h.birthday(); });
-    std::transform(people.crbegin(), people.crend(), ret_v.begin(), [](const auto& h) {
-        return !(h.isMonster()) ? 'y' : 'n';
+    std::for_each(people.begin(), people.end(), [](Human& i) { i.birthday(); });
+
+    std::transform(people.begin(), people.end(), ret_v.rbegin(), [](Human& it) {
+        if (it.isMonster()) {
+            return 'n';
+        }
+        else {
+            return 'y';
+        }
     });
 
     return ret_v;
